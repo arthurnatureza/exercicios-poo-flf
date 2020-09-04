8dbb7080d8dfb811e6eb8a0f7d2f474af6843516
Arthur Natureza <arthurnatureza@gmail.com>
2020-09-04 17:07:26 -0300

    Coloquei comentários para a média

--- CalculoFinancasPessoais/src/calculofinancaspessoais/GastosDoMes.java
+++ CalculoFinancasPessoais/src/calculofinancaspessoais/GastosDoMes.java
@@ -14,6 +14,8 @@ public class GastosDoMes {
         int gastoTotal = gastosAlimentacao + gastoSaude + gastoTransporte + gastoLazer;
         System.out.printf("Gasto total do mês: R$%d \n", gastoTotal);
         
+        //Para calcular médias devo usar o float, pois o resultado pode ser um 
+        //número real, com diversas casa decimais a direita do ponto, o que exige uma variável float
         float mediaDeGastos = (gastoTotal/4);
         System.out.printf("Média dos Gastos: R$%.2f \n", mediaDeGastos);
         
