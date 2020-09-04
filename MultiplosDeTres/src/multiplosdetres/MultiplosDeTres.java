6f6056e95c4ba1beed0f26b31cc681791d2f7544
Arthur Natureza <arthurnatureza@gmail.com>
2020-09-04 16:28:09 -0300

    Múltiplos de 3 de 1 a 100 questão 5 lista de exercícios 1

---
+++ MultiplosDeTres/src/multiplosdetres/MultiplosDeTres.java
@@ -0,0 +1,23 @@
+
+package multiplosdetres;
+
+/**
+ *
+ * @author arthurnatureza
+ */
+public class MultiplosDeTres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("0");
+        for(int i = 1; i<=100; ++i){
+            if (i%3==0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
