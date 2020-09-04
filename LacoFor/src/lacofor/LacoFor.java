a3b05b5e3d74c7496b87d8553462669df3d5259c
Arthur Natureza <arthurnatureza@gmail.com>
2020-09-04 14:56:14 -0300

    Laço for mostrando de 10 até 300, questão 3 lista exercícios 1

---
+++ LacoFor/src/lacofor/LacoFor.java
@@ -0,0 +1,17 @@
+
+package lacofor;
+
+/**
+ *
+ * @author arthurnatureza
+ */
+public class LacoFor {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 10; i<=300; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
